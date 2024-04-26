b10701253e2a3d2ba896c038e69736aadaa04935
suraj <suraj@funnelstory.io>
2024-04-26 18:44:27 +0530

    fix extended user type

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,14 +1,14 @@
-// import NextAuth, { type DefaultSession } from "next-auth";
-// import { USER_ROLE } from "./drizzle/schema";
+import NextAuth, { type DefaultSession } from "next-auth";
+import { USER_ROLE } from "./drizzle/schema";
 
-// export type ExtendedUser = DefaultSession["user"] & {
-//   role: USER_ROLE;
-//   isTwoFactorEnabled: boolean;
-//   isOAuth: boolean;
-// };
+export type ExtendedUser = DefaultSession["user"] & {
+  role: USER_ROLE;
+  isTwoFactorEnabled: boolean;
+  isOAuth: boolean;
+};
 
-// declare module "@auth/core" {
-//   interface Session {
-//     user: ExtendedUser;
-//   }
-// }
+declare module "@auth/core" {
+  interface Session {
+    user: ExtendedUser;
+  }
+}
